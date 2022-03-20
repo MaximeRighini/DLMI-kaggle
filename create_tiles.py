f714973db2cb4f2f124629176c75f561fd6106d3
MaximeRighini <201.righini.maxime@gmail.com>
2022-03-20 20:23:26 +0100

    Add slide exploration (dataviz...) and update create_tiles accordingly

--- create_tiles.py
+++ create_tiles.py
@@ -5,9 +5,6 @@ import cv2
 import skimage.io
 from tqdm import tqdm
 
-df_train = pd.read_csv("train.csv").reset_index(drop=True)
-df_test = pd.read_csv("test.csv").reset_index(drop=True)
-
 def get_tiles(img, tile_size_in, n_tiles, tile_size_out):
     # find by how much we must pad the image so that the image dims are multiples of tile_size_in
     h, w, c = img.shape
@@ -15,15 +12,15 @@ def get_tiles(img, tile_size_in, n_tiles, tile_size_out):
     pad_w = (tile_size_in - w % tile_size_in) % tile_size_in
     # pad the image with constant values (255)
     img = np.pad(img, [[pad_h//2, pad_h - pad_h//2], [pad_w//2, pad_w - pad_w//2], [0, 0]],
-                 constant_values=255)
+                 constant_values=0)
     # reshape the image and swap the order of the dims
     img = img.reshape(img.shape[0]//tile_size_in, tile_size_in, img.shape[1]//tile_size_in, tile_size_in, 3)
     img = img.transpose(0, 2, 1, 3, 4).reshape(-1, tile_size_in, tile_size_in, 3)
     # if the image has less tiles than n_tiles we pad
     if len(img) < n_tiles:
-        img = np.pad(img,[[0, n_tiles - len(img)], [0, 0], [0, 0], [0, 0]], constant_values=255)   
+        img = np.pad(img,[[0, n_tiles - len(img)], [0, 0], [0, 0], [0, 0]], constant_values=0)   
     # select the tiles with the most tissue
-    idxs = np.argsort(img.reshape(img.shape[0], -1).sum(-1))[:n_tiles]
+    idxs = np.argsort(img.reshape(img.shape[0], -1).sum(-1))[-n_tiles:]
     img = img[idxs]
     # store the tiles
     tiles = []
@@ -39,5 +36,6 @@ def add_column_tiles(df, path_imgs):
         img_id = row.image_id    
         tiff_file = os.path.join(path_imgs, f"{img_id}.tiff")
         img = skimage.io.MultiImage(tiff_file)[-1]
-        all_tiles[i] = get_tiles(img, 1024, 36, 128)
-    df["tiles"] = list(all_tiles_test.values())
+        img[img.mean(axis=-1)>=235] = 0
+        all_tiles[i] = get_tiles(img, int(np.sqrt((img!=0).sum())/14), 100, 100)
+    df["tiles"] = list(all_tiles.values())
