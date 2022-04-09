9f5c2ec95d6a088d96f43ddf3d2b92586f5565e8
MaximeRighini <201.righini.maxime@gmail.com>
2022-04-09 15:54:32 +0200

    Final pipeline

--- Kaggle DLMI - Part 2 - create_tiles.py
+++ Kaggle DLMI - Part 2 - create_tiles.py
@@ -1,3 +1,7 @@
+############################################################################################################
+#The purpose of this python file is to create the reduced images with the tiles method. We use exactly the same method as in the #exploration notebook, but we just resize the tiles to a size (128, 128, 3). We chose this size because we noticed that the higher the #resolution of the images, the better the results. A larger size would crash the colab ram.
+############################################################################################################
+
 import numpy as np
 import pandas as pd
 import os
@@ -25,22 +29,28 @@ def get_tiles(img, tile_size_in, n_tiles, tile_size_out):
     # store the tiles
     tiles = []
     for i in range(len(img)):
+        # add the tiles and reshape them
         tiles.append(cv2.resize(img[i], (tile_size_out, tile_size_out)).tolist())
     return tiles
 
-
+# We'll store the tiles in a DataFrame
 def add_column_tiles(df, path_imgs):
-    all_tiles = {}
-    for i in tqdm(range(df.shape[0])):
-        row = df.iloc[i]
-        img_id = row.image_id    
+    all_tiles = {} # This dictionary will store all the files
+    for i in tqdm(range(df.shape[0])): # Go through each index of the dataframe
+        row = df.iloc[i] # Find the row associated to that index
+        img_id = row.image_id  # Find the slide
+        # Load the slide
         tiff_file = os.path.join(path_imgs, f"{img_id}.tiff")
         img = skimage.io.MultiImage(tiff_file)[-1]
+        # Remove the background
         img[img.mean(axis=-1)>=235] = 0
+        # Create the tiles (we reshape in size (128, 128, 3)
         all_tiles[i] = get_tiles(img, int(np.sqrt((img!=0).sum())/14), 100, 128)
+    # Convert the dictionary in a dataframe column
     df["tiles"] = list(all_tiles.values())
 
 
+# Add the tiles to the train and test dataframes and export them
 df_train = pd.read_csv("train.csv").reset_index(drop=True)
 df_test = pd.read_csv("test.csv").reset_index(drop=True)
 
